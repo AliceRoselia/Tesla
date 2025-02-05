1c0e9135e726fbea21153e67b007048536b00b08
Myself <63040919+AliceRoselia@users.noreply.github.com>
2025-02-05 13:08:02 +0700

    Chess moves working in progress.

--- src/Board.jl
+++ src/Board.jl
@@ -69,12 +69,12 @@ function Board(board::String, castling_rights = (true,true,true,true))
     return Board(ourPiece,enemyPiece,pawn,knight,bishop,rook,queen,kings)
 end
 
-function get_our_king_pos(board::Board)
-    return (board.kings>>6)&63 + 1
+function get_our_king_pos(board::Board, onebasedindexing = true)
+    return (board.kings>>6)&63 + onebasedindexing
 end
 
-function get_enemy_king_pos(board::Board)
-    return (board.kings)&63 + 1
+function get_enemy_king_pos(board::Board,onebasedindexing = true)
+    return (board.kings)&63 + onebasedindexing
 end
 
 function print_board(board::Board)
@@ -97,26 +97,58 @@ function print_board(board::Board)
     end
 end
 
-mutable struct Move<:AbstractMove 
+struct pieceMove<:AbstractMove 
     data::UInt32
 end
 
+#A piece move full of information.
+#Used for debugging/etc purposes.
+#A 16-bits Partial move will probably be used instead of this in the real engine.
+
 Move(starting_square::Integer, ending_square::Integer, board::Board) = Move(UInt32(starting_square),UInt32(ending_square),board)
 
-function Move(starting_square::UInt32, ending_square::UInt32, board::Board, onebasedindexing = true)
+function pieceon(square,board::Board, onebasedindexing = true)
+    square -= onebasedindexing
+    
+    if (square == get_our_king_pos(board,false)) | (square == get_enemy_king_pos(board,false)) 
+        return KING
+    end
+    
+    return (((board.pawn>>square)&1) * PAWN) | (((board.knight>>square)&1) * KNIGHT) | (((board.bishop>>square)&1) * BISHOP) | (((board.rook>>square)&1) * ROOK) | (((board.queen>>square)&1) * QUEEN)
+end
+
+function coloron(square, board::Board, onebasedindexing = true)
+    square -= onebasedindexing
+    return (board.enemyPiece>>square)&1
+end
+
+function pieceMove(starting_square::UInt32, ending_square::UInt32, board::Board, onebasedindexing = true)
     starting_square -= onebasedindexing
     ending_square -= onebasedindexing
 
-    return Move((starting_square<<26 )| (ending_square<<20) | pieceon(starting_square,board), pieceon())
+    return pieceMove((starting_square<<26 )| (ending_square<<20) | (pieceon(starting_square,board)<<14) | (pieceon(ending_square,board))<<11)
+    #Bits 32 to 27 is starting square.
+    #Bits 26 to 21 is ending square.
+    #Bits 20 to 18 is promoted piece type.
+    #Bits 17 to 15 is the moving piece type.
+    #Bits 14 to 12 is attacked type
+    #Bits 13 to 10 one hots encode the four main castle.
+    #Bits 9 for en passant marking.
+    #The rest are reserved for future purposes.
 end
 
 function promote_Move(starting_square::UInt32, ending_square::UInt32, board::Board,promotedPiece = QUEEN, onebasedindexing = true)
+    return pieceMove(pieceMove(starting_square,ending_square,board,onebasedindexing).data | (promotedPiece<<17))
+end
 
-function is_legal(move::Move, board::Board)
+
+function is_legal(move::chessMove, board::Board)
     #An expensive check whether or not the move is legal.
-    working in progress.
+    #working in progress.
 end
 
-function is_not_suicide(move::Move, board::Board)
+function is_not_suicide(move::chessMove, board::Board)
     #Assumes the move is otherwise legal. Doesn't put yourself in check.
+    # Hmm
 end
+
